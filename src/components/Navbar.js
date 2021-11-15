8951b52ceb4a65af29f742772f50ea7b61daa3a3
sammwaura <meshsami27@gmail.com>
2021-11-15 15:30:55 +0300

    add files

---
+++ src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+ 
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+    return(
+        <>
+        <nav className="navbar">
+          <div className="navbar-container">
+            <Link to="/" classname="navbar-logo">
+                E <i className="fab fa-typo3"></i>
+            </Link>
+            <div className="menu-icon" onClick={handleClick}>
+                <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                <li className="nav-item">
+                    <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+
+                    </Link>
+                </li>
+
+            </ul>
+        </div>
+        </nav>
+        </>
+    )
+}
+
+export default Navbar 
