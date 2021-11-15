0d3fcf25eda559c44b27db005536e9d2f3bd856b
sammwaura <meshsami27@gmail.com>
2021-11-15 17:47:54 +0300

    modify files

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ function Navbar() {
         <nav className="navbar">
           <div className="navbar-container">
             <Link to="/" classname="navbar-logo">
-                E <i className="fab fa-typo3"></i>
+              <i class="fab fa-etsy"></i>   uropa Health Care 
             </Link>
             <div className="menu-icon" onClick={handleClick}>
                 <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
